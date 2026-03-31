3a13a0ed70da2a656af2b00bbed651cd6fc55ce1
emanueldias01 <emanueldias01dev@gmail.com>
2026-03-31 14:32:56 -0300

    implements POJOs class

---
+++ src/main/java/br/com/sd/entitys/Board.java
@@ -0,0 +1,28 @@
+package br.com.sd.entitys;
+
+public class Board {
+
+    private int height;
+    private int width;
+
+    public Board(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
